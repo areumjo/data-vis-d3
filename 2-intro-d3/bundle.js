1bacc221d82498e2219cb8c526d7ea5fe0799dac
Areum Jo <areumjo1@gmail.com>
2019-10-11 20:03:19 -0700

    Imported d3 function using ES6

---
+++ 2-intro-d3/bundle.js
@@ -0,0 +1,4 @@
+(function (d3) {
+    const svg = d3.select('svg');
+    svg.style('background-color', 'green');
+}(d3));
---
+++ 2-intro-d3/index.js
@@ -0,0 +1,5 @@
+import { select } from 'd3';
+// use ES6 syntax to import select module from d3
+
+const svg = select('svg');
+svg.style('background-color', 'blue');
