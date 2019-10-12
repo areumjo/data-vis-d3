b1912d8ba5edec154e4de9f4984695533a5afdb6
Areum Jo <areumjo1@gmail.com>
2019-10-11 20:25:04 -0700

    Made a smiley face with circles and arc

--- 2-intro-d3/bundle.js
+++ 2-intro-d3/bundle.js
@@ -1,4 +1,39 @@
 (function (d3) {
     const svg = d3.select('svg');
-    svg.style('background-color', 'green');
+
+    const width = parseFloat(svg.attr('width')); //960 as a string, need to convert to num
+    const height = +svg.attr('height'); //500, + mean same as parseFloat
+    
+    const circle = svg.append('circle')
+        .attr('r', height / 2)
+        .attr('cx', width / 2 )
+        .attr('cy', height / 2)
+        .attr('fill', 'yellow')
+        .attr('stroke', 'black');
+
+    const eyeSpacing = 100;
+    const eyeYOffset = -70;
+
+    const leftEye = svg.append('circle')
+        .attr('r', 30)
+        .attr('cx', width / 2 - eyeSpacing)
+        .attr('cy', height / 2 + eyeYOffset)
+        .attr('fill', 'black');
+    
+    const rightEye = svg.append('circle')
+        .attr('r', 30)
+        .attr('cx', width / 2 + eyeSpacing)
+        .attr('cy', height / 2 + eyeYOffset)
+        .attr('fill', 'black');
+    
+    const g = svg.append('g')
+        .attr('transform', `translate(${width/2}, ${height/2})`);
+    
+    const mouth = g.append('path')
+        .attr('d', d3.arc()({
+            innerRadius: 150,
+            outerRadius: 170,
+            startAngle: Math.PI / 2,
+            endAngle: Math.PI * 3 / 2
+        }));
 }(d3));
--- 2-intro-d3/index.js
+++ 2-intro-d3/index.js
@@ -1,5 +1,43 @@
-import { select } from 'd3';
+import { select, arc } from 'd3';
 // use ES6 syntax to import select module from d3
 
 const svg = select('svg');
-svg.style('background-color', 'blue');
+
+// can extract value from index.html
+const width = parseFloat(svg.attr('width')); //960 as a string, need to convert to num
+const height = +svg.attr('height'); //500, + mean same as parseFloat
+// console.log(typeof width, 'hey:', width); //num
+
+// d3 method chaining
+const circle = svg.append('circle')
+    .attr('r', height / 2)
+    .attr('cx', width / 2 )
+    .attr('cy', height / 2)
+    .attr('fill', 'yellow')
+    .attr('stroke', 'black');
+
+const eyeSpacing = 100;
+const eyeYOffset = -70;
+
+const leftEye = svg.append('circle')
+    .attr('r', 30)
+    .attr('cx', width / 2 - eyeSpacing)
+    .attr('cy', height / 2 + eyeYOffset)
+    .attr('fill', 'black');
+
+const rightEye = svg.append('circle')
+    .attr('r', 30)
+    .attr('cx', width / 2 + eyeSpacing)
+    .attr('cy', height / 2 + eyeYOffset)
+    .attr('fill', 'black');
+
+const g = svg.append('g')
+    .attr('transform', `translate(${width/2}, ${height/2})`);
+
+const mouth = g.append('path')
+    .attr('d', arc()({
+        innerRaius: 150,
+        outerRadius: 170,
+        startAngle: 0,
+        endAngle: Math.PI * 3/2
+    }))
