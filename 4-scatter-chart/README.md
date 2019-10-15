efe5c8d7de3270bd4b5492d450a641baf1dbf5f0
Areum Jo <areumjo1@gmail.com>
2019-10-15 11:15:59 -0700

    Data fetched and displayed as circles with scaleLinear

---
+++ 4-scatter-chart/README.md
@@ -0,0 +1,20 @@
+## Making scatter plot with d3.js
+
+### Data
+- [Using this data format](https://vizhub.com/curran/datasets/auto-mpg.csv), data from [UCI Machine Learning Repository: Auto MPG Data Set](http://mlr.cs.umass.edu/ml/datasets/Auto+MPG)
+```js
+// when you fetch data
+// columns: (9) ["mpg", "cylinders", "displacement", "horsepower", "weight", "acceleration", "year", "origin", "name"]
+// length: 392
+{
+  acceleration: "12"
+  cylinders: "8"
+  displacement: "307"
+  horsepower: "130"
+  mpg: "18"
+  name: "chevrolet chevelle malibu"
+  origin: "USA"
+  weight: "3504"
+  year: "1970"
+}
+```
---
+++ 4-scatter-chart/index.js
@@ -0,0 +1,51 @@
+const svg = d3.select('svg');
+
+const width = +svg.attr('width');
+const height = +svg.attr('height');
+
+const render = data => {
+
+  const xValue = d => d.horsepower;
+  const yValue = d => d.weight;
+  const circleRadius = 8;
+
+  const margin = { top: 100, right: 100, bottom: 100, left: 100 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(data, xValue))
+    .range([0, innerWidth])
+    .nice();
+
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(data, yValue))
+    .range([innerHeight, 0])
+    .nice();
+
+  const g = svg.append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+  g.selectAll('circle').data(data)
+    .enter().append('circle')
+      .attr('cy', d => yScale(yValue(d)))
+      .attr('cx', d => xScale(xValue(d)))
+      .attr('r', circleRadius);
+}
+
+
+d3.csv('https://vizhub.com/curran/datasets/auto-mpg.csv')
+  .then(data => {
+    // console.log(data);
+    // console.log(data[0]);
+    data.forEach(d => {
+      d.mpg = +d.mpg;
+      d.cylinders = +d.cylinders;
+      d.displacement = +d.displacement;
+      d.horsepower = +d.horsepower;
+      d.weight = +d.weight;
+      d.acceleration = +d.acceleration;
+      d.year = +d.year;  
+    });
+  render(data);
+});
