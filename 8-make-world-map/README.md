70fdf07b4524b394a037270c52aa8c2e9b421ed7
Areum Jo <areumjo1@gmail.com>
2019-10-16 11:32:13 -0700

    Interative world map

--- 8-make-world-map/README.md
+++ 8-make-world-map/README.md
@@ -33,9 +33,46 @@ console.log(data.objects.countries);
   - an extension of GeoJSON that endoes topology
   - geometries in TopoJSON files are stitched together from shared lie segments called `arcs`
 
+### Interative tooltip for country name
+- Use `d3.tsv()` to get country name for each country id
+  - out of 67 columns, country name for `name` and country id for `iso_n3`
+- Use `Promise.all` to get data from different files
+  - can change resolution of the map by applying different number in the url like `110m` to `50m` (better resolution)
+```js
+Promise.all([
+  d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv'),
+  d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
+]).then(([tsvData, topojsonData]) => {
+  const countryName = {};
+  tsvData.forEach(d => {
+    countryName[d.iso_n3] = d.name;
+  })
+  const countries = topojson.feature(topojsonData, topojsonData.objects.countries);
+    g.selectAll('path').data(countries.features)
+      .enter().append('path')
+        .attr('class', "country")
+        .attr('d', pathGenerator)
+      .append('title')
+        .text(d => countryName[d.id]);
+})
+```
+
+### Panning & Zooming
+- Now map is able to zoom in and out
+```js
+svg.call(d3.zoom().on('zoom', () => {
+  g.attr('transform', d3.event.transform);
+}));
+```
+
 ### Projection
 - `d3.geoEquirectangular()`
 ![](8-square-map.png)
 
 - `d3.geoNaturalEarth1()`
 ![](8-sphere-map.png)
+
+### End result
+- Map will show the country name after you hover over it
+![](8-1-highlighting-map.png)
+![](8-2-zoom-in-map.png)
--- 8-make-world-map/index.js
+++ 8-make-world-map/index.js
@@ -3,13 +3,6 @@ const svg = d3.select('svg');
 const projection = d3.geoEquirectangular();
 const pathGenerator = d3.geoPath().projection(projection);
 
-// title
-d3.select('body').append('text')
-  .attr('class', 'title')
-  .attr('x', 0)
-  .attr('y', 0)
-  .text('World map')
-
 // new group
 const g = svg.append('g');
 
@@ -17,14 +10,46 @@ g.append('path')
   .attr('class', 'sphere')
   .attr('d', pathGenerator({type: 'Sphere'}));
 
-d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
-  .then(data => {
-    // console.log(data);
-    // console.log(data.objects.countries); // "GeometryCollection"
-    const countries = topojson.feature(data, data.objects.countries);
-    // console.log({countries}); // "FeatureCollection"
-    svg.selectAll('path').data(countries.features)
+// zoom
+svg.call(d3.zoom().on('zoom', () => {
+  g.attr('transform', d3.event.transform);
+}));
+
+// use `Promise.all` to get data from different files
+Promise.all([
+  d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv'),
+  d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
+]).then(([tsvData, topojsonData]) => {
+  // grab countryName from tsvData and will add it to tooltip
+  const countryName = {};
+  tsvData.forEach(d => {
+    countryName[d.iso_n3] = d.name;
+  })
+  const countries = topojson.feature(topojsonData, topojsonData.objects.countries);
+    g.selectAll('path').data(countries.features)
       .enter().append('path')
         .attr('class', "country")
-        .attr('d', pathGenerator);
-});
+        .attr('d', pathGenerator)
+      .append('title')
+        // .text(d => console.log(countryName[d.id]))
+        .text(d => countryName[d.id]);
+})
+
+// we need to grab country name but this json doesn't have that info ==> use `tsv`
+
+// d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv')
+//   .then(data => {
+//     // console.log(data);
+//   })
+
+// d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
+//   .then(data => {
+//     // console.log(data);
+//     // console.log(data.objects.countries); // "GeometryCollection"
+//     const countries = topojson.feature(data, data.objects.countries);
+//     // console.log({countries}); // "FeatureCollection"
+//     svg.selectAll('path').data(countries.features)
+//       .enter().append('path')
+//         .attr('class', "country")
+//         .attr('d', pathGenerator);
+// });
